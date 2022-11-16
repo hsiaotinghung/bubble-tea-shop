5083cc78917714d5e5533b4f155208d69e19edf4
Hsiaoting Hung <hung.hsiaoting@gmail.com>
2022-11-15 19:41:47 -0500

    Add cart context and use it

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Cart from "./pages/Cart";
 import Order from "./pages/Order";
 import NotFound from "./pages/NotFound";
 import Layout from "./layouts/Layout";
+import CartProvider from "./context/CartProvider";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/order" />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <CartProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/order" />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </CartProvider>
   );
 }
 
